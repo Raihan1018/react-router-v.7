7a7bed3f431e11d629c787c0cf3b714fb6d09825
Raihan <raihanuddinahmed10@gmail.com>
2025-09-16 01:33:19 +0600

    nav

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Header from '../components/Header'
-import { Outlet } from 'react-router'
+import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router";
+import Footer from "../components/Footer";
+import SideBar from "../components/SideBar";
 
 const Root = () => {
   return (
     <div>
-        <Header/>
-        <Outlet/>
+      <Header />
+      <div>
+        <SideBar />
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <h2>This is header</h2>
+      <nav className="px-5">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-blue-500"
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-black"
+          }
+          to="/mobiles"
+        >
+          Mobiles
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-black"
+          }
+          to="/laptops"
+        >
+          Laptop
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
